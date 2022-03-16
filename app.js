cd15ba6b15cbd6865e7568d34322537aa35bc5fa
emreissvr <80003871+emreissvr@users.noreply.github.com>
2022-03-17 02:10:02 +0300

    refactoring1

--- app.js
+++ app.js
@@ -2,9 +2,26 @@ const express = require('express');
 
 const app = express();
 
+// Template Engine 
+app.set("view engine", "ejs");
+
+// middlewares
+app.use(express.static("public"));
+
+// routes
+// index
 app.get("/",(req,res) =>{
-    res.send("index sayfası");
+    res.status(200).render("index",{
+        page_name :"index"
+    });
 })
+// about
+app.get("/about",(req,res) =>{
+    res.status(200).render("about",{
+        page_name: "about"
+    });
+})
+
 
 
 
